2a9d7cbd4e47a652aab0c399fade58b821010201
josepfpinto <josepfpinto@gmail.com>
2024-02-28 21:30:51 +0000

    initial setup for whatsapp

--- lib/whatsapp.py
+++ lib/whatsapp.py
@@ -1,4 +1,5 @@
 """Whatsapp related functions"""
+import json
 import os
 from dotenv import load_dotenv
 import requests
@@ -6,7 +7,38 @@ import requests
 # Load environment variables
 load_dotenv()
 WHATSAPP_TOKEN = os.getenv('WHATSAPP_TOKEN')
-WHATSAPP_URL = os.getenv('WHATSAPP_URL')
+WHATSAPP_RECIPIENT_WAID = os.getenv("WHATSAPP_RECIPIENT_WAID")
+WHATSAPP_PHONE_NUMBER_ID = os.getenv("WHATSAPP_PHONE_NUMBER_ID")
+WHATSAPP_VERSION = os.getenv("WHATSAPP_VERSION")
+WHATSAPP_APP_ID = os.getenv("WHATSAPP_APP_ID")
+WHATSAPP_APP_SECRET = os.getenv("WHATSAPP_APP_SECRET")
+
+# 22-jul-2023 - WhatsApp supported types
+MEDIA_TYPES = {
+    'audio/aac': 'aac',
+    'audio/mp4': 'mp4',
+    'audio/mpeg': 'mp3',
+    'audio/amr': 'amr',
+    'audio/ogg': 'ogg',
+    'video/mp4': 'mp4',
+    'video/3gp': '3gp',
+    'image/jpeg': 'jpeg',
+    'image/png': 'png',
+    'image/webp': 'webp',
+    'text/plain': 'txt',
+    'application/pdf': 'pdf',
+    'application/vnd.ms-powerpoint': 'ppt',
+    'application/msword': 'doc',
+    'application/vnd.ms-excel': 'xls',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+}
+
+whatsapp_url = f"https://graph.facebook.com/{
+    WHATSAPP_VERSION}/{WHATSAPP_PHONE_NUMBER_ID}/messages"
+headers = {'Content-Type': 'application/json',
+           'Authorization': 'Bearer ' + WHATSAPP_TOKEN}
 
 
 def get_message(message):
@@ -25,13 +57,42 @@ def get_message(message):
     return text, media_id
 
 
-def send_message(data):
+def send_template_message(recipient, template="hello_world"):
+    """send template message to Whatsapp"""
+    try:
+        data = {
+            "messaging_product": "whatsapp",
+            "to": recipient,
+            "type": "template",
+            "template": {"name": template, "language": {"code": "en_US"}},
+        }
+        print("sending... ", data, whatsapp_url)
+        data_json = json.dumps(data)
+        response = requests.post(whatsapp_url,
+                                 headers=headers,
+                                 data=data_json,
+                                 timeout=20)
+
+        if response.status_code == 200:
+            return 'message sent', 200
+        else:
+            return 'error while sending message', response.json()
+    except Exception as e:
+        return e, 403
+
+
+def send_message(recipient, data):
     """send message to Whatsapp"""
     try:
-        whatsapp_token = WHATSAPP_TOKEN
-        whatsapp_url = WHATSAPP_TOKEN
-        headers = {'Content-Type': 'application/json',
-                   'Authorization': 'Bearer ' + whatsapp_token}
+        data = json.dumps(
+            {
+                "messaging_product": "whatsapp",
+                "recipient_type": "individual",
+                "to": recipient,
+                "type": "text",
+                "text": {"preview_url": False, "body": data},
+            }
+        )
         print("sending... ", data)
         response = requests.post(whatsapp_url,
                                  headers=headers,
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-from lib.gpt import translate_with_gpt4_langchain
+"""Testing"""
+import os
+from lib.whatsapp import send_template_message
+from dotenv import load_dotenv
 
+load_dotenv()
 cost_info = []
 DUMMY_MESSAGE = "Eu chamo-me José"
 
-message = translate_with_gpt4_langchain(DUMMY_MESSAGE, cost_info)
-print(message)
-print(cost_info)
+WHATSAPP_RECIPIENT_WAID = os.getenv("WHATSAPP_RECIPIENT_WAID")
+print(send_template_message(WHATSAPP_RECIPIENT_WAID))
