9bc3e1b1af4ad69829b29fa6fe0b5c3b7485957c
josepfpinto <josepfpinto@gmail.com>
2024-03-21 23:16:04 +0000

    add langchain smith

--- lib/websearch_tool.py
+++ lib/websearch_tool.py
@@ -15,11 +15,12 @@ SERPER_API_KEY = os.getenv('SERPER_API_KEY')
 BRAVE_API_KEY = os.getenv('BRAVE_API_KEY')
 
 # Setup for LangChain
-search_tavily = TavilySearchResults(api_wrapper=TavilySearchAPIWrapper())
+search_tavily = TavilySearchResults(
+    api_wrapper=TavilySearchAPIWrapper(), max_results=4)
 search_serper = Tool(
     name="Google Search",
     func=GoogleSerperAPIWrapper(serper_api_key=SERPER_API_KEY).results,
-    description="Access to google search. Use this to obtain information about current events, to find support links, or to get info on how to write code."
+    description="Access to google search. Use this to obtain information about current events, to find support links, or to get info on how to write code.",
 )
 search_brave = tool = BraveSearch.from_api_key(
-    api_key=BRAVE_API_KEY, search_kwargs={"count": 3})
+    api_key=BRAVE_API_KEY, search_kwargs={"count": 4})
